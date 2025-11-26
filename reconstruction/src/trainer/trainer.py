26a800df851c89a4a50779db58d44e51926f69bc
Adrian Riadi <34883039+corvus-rex@users.noreply.github.com>
2025-11-26 11:52:33 +0700

    Resolve 'InpaintGenerator' object has no attribute 'module' error

--- reconstruction/src/trainer/trainer.py
+++ reconstruction/src/trainer/trainer.py
@@ -78,10 +78,10 @@ class Trainer:
         if self.args.global_rank == 0:
             print(f"\nsaving {self.iteration} model to {self.args.save_dir} ...")
             torch.save(
-                self.netG.module.state_dict(), os.path.join(self.args.save_dir, f"G{str(self.iteration).zfill(7)}.pt")
+                self.netG.state_dict(), os.path.join(self.args.save_dir, f"G{str(self.iteration).zfill(7)}.pt")
             )
             torch.save(
-                self.netD.module.state_dict(), os.path.join(self.args.save_dir, f"D{str(self.iteration).zfill(7)}.pt")
+                self.netD.state_dict(), os.path.join(self.args.save_dir, f"D{str(self.iteration).zfill(7)}.pt")
             )
             torch.save(
                 {"optimG": self.optimG.state_dict(), "optimD": self.optimD.state_dict()},
